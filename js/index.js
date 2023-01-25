0734ca2df046fedba05caee0eaf9c8d828008958
Farid <beatsbyfar.id@gmail.com>
2023-01-24 16:20:14 -0800

    Added main page functionality

---
+++ js/index.js
@@ -0,0 +1,39 @@
+import { blogPosts } from "./data.js"
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".nav-menu")
+let viewMore = false
+
+hamburger.addEventListener("click", function(){
+    hamburger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+})
+
+function renderPosts(initialPosition, lastPosition){
+    for (let i = initialPosition; i < lastPosition; i++){
+        document.querySelector(".posts").innerHTML += 
+            `
+            <div class="post">
+                <img src=${blogPosts[i].image} alt=${blogPosts[i].alt}>
+                <p class="date">${blogPosts[i].date}</p>
+                <p class="blog-title">${blogPosts[i].title}</p>
+                <p class="desc">${blogPosts[i].desc}</p>
+            </div>
+            `
+    }
+}
+
+renderPosts(0, 6)
+document.addEventListener("click", function(e){
+    if (e.target.classList.contains("view-more-btn")){
+        if (!viewMore){
+            renderPosts(6, blogPosts.length)
+            document.querySelector(".view-more-btn").textContent = 'View Less'
+        }
+        else {
+            document.querySelector(".posts").innerHTML = ``
+            renderPosts(0, 6)
+            document.querySelector(".view-more-btn").textContent = 'View More'
+        }
+        viewMore = !viewMore
+    }
+})
