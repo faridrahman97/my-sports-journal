fac5257eb853e1b0f5bad6527c453ed66b03bfe4
Farid <beatsbyfar.id@gmail.com>
2023-01-25 15:42:49 -0800

    Complete all functionality

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 export const blogPosts = [
     {
         title: "My Trip to India",
-        image: "images/tajmahal.jpg",
+        image: "https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
         alt: "An image of the Taj Mahal",
         date: "January 15, 2023",
         desc: "Since my family origins come from India, I was very much interested to travel there. \
@@ -9,60 +9,60 @@ export const blogPosts = [
     },
     {
         title: "Catch Me in Japan",
-        image: "images/tokyo.jpg",
+        image: "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=894&q=80",
         alt: "An image of Tokyo",
         date: "December 24, 2022",
         desc: "I first went to Japan in high school. I loved it there. An incredible time and the people are so wonderful. The food is amazing and I can't wait to go again."
     },
     {
         title: "Swiss Cheese Please",
-        image: "images/switzerland.jpg",
+        image: "https://images.unsplash.com/photo-1530122037265-a5f1f91d3b99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
         alt: "An image of a hill in Switzerland",
         date: "December 12, 2022",
         desc: "Switzerland is a place that is on my bucket list. I would love to head there and check out all the cool scenes and places. It seems so peaceful and naturally beautiful."
     },
     {
-        title: "My Trip to India",
-        image: "images/tajmahal.jpg",
-        alt: "An image of the Taj Mahal",
-        date: "January 15, 2023",
-        desc: "Since my family origins come from India, I was very much interested to travel there. \
-        Finally, I was able to make the trip. The food was phenomenal and the views were to die for!"
+        title: "London is Beautiful",
+        image: "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+        alt: "An image of London",
+        date: "December 5, 2022",
+        desc: "I've always wanted to go to London. \
+        I believe I went once as a kid, but unfortunately I don't remember much. Hopefully, I will be going there soon."
     },
     {
-        title: "Catch Me in Japan",
-        image: "images/tokyo.jpg",
-        alt: "An image of Tokyo",
-        date: "December 24, 2022",
-        desc: "I first went to Japan in high school. I loved it there. An incredible time and the people are so wonderful. The food is amazing and I can't wait to go again."
+        title: "O Canada",
+        image: "https://images.unsplash.com/photo-1508693926297-1d61ee3df82a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        alt: "An image of a Canadian maple leaf.",
+        date: "November 25, 2022",
+        desc: "I first went to Canada as a kid since I have family from there. The people, the places, and the nature are all so gorgeous."
     },
     {
-        title: "Swiss Cheese Please",
-        image: "images/switzerland.jpg",
-        alt: "An image of a hill in Switzerland",
-        date: "December 12, 2022",
-        desc: "Switzerland is a place that is on my bucket list. I would love to head there and check out all the cool scenes and places. It seems so peaceful and naturally beautiful."
+        title: "Yosemite, I Love You",
+        image: "https://images.unsplash.com/photo-1498429089284-41f8cf3ffd39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        alt: "An image of mountains in Yosemite.",
+        date: "November 12, 2022",
+        desc: "I went to Yosemite once as a kid with my uncles. I loved taking pictures here and hope I get to go again soon."
     },
     {
-        title: "My Trip to India",
-        image: "images/tajmahal.jpg",
-        alt: "An image of the Taj Mahal",
-        date: "January 15, 2023",
-        desc: "Since my family origins come from India, I was very much interested to travel there. \
-        Finally, I was able to make the trip. The food was phenomenal and the views were to die for!"
+        title: "NY State of Mind",
+        image: "https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        alt: "An image of Times Square in New York.",
+        date: "August 13, 2022",
+        desc: "I've never actually been to New York, but it is definitely on my bucket list. \
+        I have dreamt of walking through Times Square and seeing all the city lights for so long!"
     },
     {
-        title: "Catch Me in Japan",
-        image: "images/tokyo.jpg",
-        alt: "An image of Tokyo",
-        date: "December 24, 2022",
-        desc: "I first went to Japan in high school. I loved it there. An incredible time and the people are so wonderful. The food is amazing and I can't wait to go again."
+        title: "Sweet Home Colorado",
+        image: "https://images.unsplash.com/photo-1620248742445-ce3a0de4b2b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=868&q=80",
+        alt: "An image of Larimer Square in Denver.",
+        date: "August 11, 2022",
+        desc: "I moved to Colorado in 2021 and have loved the area ever since. It's totally breathtaking. So many things to do if you're an outdoorsy person!"
     },
     {
-        title: "Swiss Cheese Please",
-        image: "images/switzerland.jpg",
-        alt: "An image of a hill in Switzerland",
-        date: "December 12, 2022",
-        desc: "Switzerland is a place that is on my bucket list. I would love to head there and check out all the cool scenes and places. It seems so peaceful and naturally beautiful."
+        title: "The Bay Area: My Birthplace",
+        image: "https://images.unsplash.com/photo-1501594907352-04cda38ebc29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
+        alt: "An image of the Golden Gate Bridge.",
+        date: "July 12, 2022",
+        desc: "The Bay Area is where I was born and raised so of course, I had to start things off with San Francisco! This town is amazing for it's food, culture, and places to see. The Golden Gate Bridge is one of the coolest bridges in the entire world!"
     },
 ]
